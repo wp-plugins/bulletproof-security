abb2bb6a08b03619f33f3f2e2f50111fc3420aa5
AITpro <AITpro@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-08 22:27:28 +0000

    Reverting to .46.2 until error is found
    git-svn-id: https://plugins.svn.wordpress.org/bulletproof-security/trunk@394801 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/includes/admin.php
+++ admin/includes/admin.php
@@ -6,11 +6,14 @@ if (!function_exists ('add_action')) {
 		exit();
 }
 
+// WordPress "Settings" Panel menu link
+function bulletproof_security_admin_link() {
+	add_options_page(__('BulletProof Security', 'bulletproof-security'), __('BulletProof Security', 'bulletproof-security'), 'manage_options', 'bulletproof-security/admin/options.php');
+}
+
 function bulletproof_security_admin_init() {
-	// whitelist BPS DB options 
-	register_setting('bulletproof_security_options', 'bulletproof_security_options', 'bulletproof_security_options_validate');
-	register_setting('bulletproof_security_options_maint', 'bulletproof_security_options_maint', 'bulletproof_security_options_validate_maint');
-	register_setting('bulletproof_security_options_mynotes', 'bulletproof_security_options_mynotes', 'bulletproof_security_options_validate_mynotes');
+	// Global 'whitelist_settings' register
+	register_setting('bulletproof_security', 'bulletproof_security', 'bulletproof_security_save_settings');
 	
 	// Register BPS js
 	wp_register_script( 'bps-js', WP_PLUGIN_URL . '/bulletproof-security/admin/js/bulletproof-security-admin.js');
@@ -28,15 +31,11 @@ function bulletproof_security_admin_init() {
 	add_action('load-bulletproof-security/admin/options.php', 'bulletproof_security_load_settings_page');
 }
 	
-// WordPress "Settings" Panel menu link
-function bulletproof_security_admin_link() {
-	add_options_page(__('BulletProof Security ~ .htaccess Core', 'bulletproof-security'), __('BulletProof Security', 'bulletproof-security'), 'manage_options', 'bulletproof-security/admin/options.php');
-}
-
-// Enqueue BPS scripts and styles only on BPS Options page
 function bulletproof_security_load_settings_page() {
-	global $bulletproof_security;
-	
+	global $bulletproof_security, $user_ID;
+	do_action('bps_settings_page');
+
+	// Enqueue BPS scripts and styles
 	wp_enqueue_script('jquery');
 	wp_enqueue_script('jquery-ui-tabs');
 	wp_enqueue_script('jquery-form');
@@ -51,48 +50,13 @@ function bulletproof_security_install() {
 	global $bulletproof_security;
 	$previous_install = get_option('bulletproof_security');
 	if ( $previous_install ) {
-	if ( version_compare($previous_install['version'], '.46.3', '<') )
+	if ( version_compare($previous_install['version'], '.46.2', '<') )
 	remove_role('denied');
 	}
 }
 
 function bulletproof_security_uninstall() {
-	require_once( ABSPATH . 'wp-admin/includes/plugin.php');
 	$options = get_option('bulletproof_security');
-	delete_option('bulletproof_security_options');
-	delete_option('bulletproof_security_options_maint');
-	delete_option('bulletproof_security_options_mynotes');
-}
-
-// Validate BPS options
-function bulletproof_security_options_validate($input) {  
-	$options = get_option('bulletproof_security_options');  
-		
-	return $options;  
-}
-
-// Validate BPS options - Maintenance Mode Form 
-function bulletproof_security_options_validate_maint($input) {  
-	$options = get_option('bulletproof_security_options_maint');  
-	$options['bps-site-title'] = wp_filter_nohtml_kses($input['bps-site-title']);
-	$options['bps-message-1'] = wp_filter_nohtml_kses($input['bps-message-1']);
-	$options['bps-message-2'] = wp_filter_nohtml_kses($input['bps-message-2']);
-	$options['bps-start-date'] = wp_filter_nohtml_kses($input['bps-start-date']);
-	$options['bps-start-time'] = wp_filter_nohtml_kses($input['bps-start-time']);
-	$options['bps-end-date'] = wp_filter_nohtml_kses($input['bps-end-date']);
-	$options['bps-end-time'] = wp_filter_nohtml_kses($input['bps-end-time']);
-	$options['bps-popup-message'] = wp_filter_nohtml_kses($input['bps-popup-message']);
-	$options['bps-retry-after'] = wp_filter_nohtml_kses($input['bps-retry-after']);
-	$options['bps-background-image'] = wp_filter_nohtml_kses($input['bps-background-image']);
-		
-	return $options;  
-}
-
-// Validate BPS options - BPS "My Notes" settings 
-function bulletproof_security_options_validate_mynotes($input) {  
-	$options = get_option('bulletproof_security_options_mynotes');  
-	$options['bps_my_notes'] = htmlspecialchars($input['bps_my_notes']);
-		
-	return $options;  
+	require_once( ABSPATH . 'wp-admin/includes/plugin.php');
 }
 ?>
--- bulletproof-security.php
+++ bulletproof-security.php
@@ -3,7 +3,7 @@
 Plugin Name: BulletProof Security
 Plugin URI: http://www.ait-pro.com/aitpro-blog/297/bulletproof-security-plugin-support/bulletproof-security-wordpress-plugin-support/
 Description: Website Security Protection: BulletProof Security protects your website from XSS, CSRF, Base64_encode and SQL Injection hacking attempts. One-click .htaccess WordPress security protection. Protects wp-config.php, bb-config.php, php.ini, php5.ini, install.php and readme.html with .htaccess security protection. One-click Website Maintenance Mode(HTTP 503). Additional website security checks: DB errors off, file and folder permissions check... System Info: PHP, MySQL, OS, Memory Usage, IP, Max file sizes... Built-in .htaccess file editing, uploading and downloading.
-Version: .46.3
+Version: .46.2
 Author: Edward Alexander
 Author URI: http://www.ait-pro.com/
 */
@@ -25,7 +25,7 @@ Author URI: http://www.ait-pro.com/
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-define( 'BULLETPROOF_VERSION', '.46.3' );
+define( 'BULLETPROOF_VERSION', '.46.2' );
 
 // Global configuration class file - pending development
 require_once( WP_PLUGIN_DIR . '/bulletproof-security/includes/class.php' );
@@ -53,7 +53,7 @@ if ( is_admin() ) {
     register_uninstall_hook(__FILE__, 'bulletproof_security_uninstall');
 
 	add_action( 'admin_init', 'bulletproof_security_admin_init' );
-    add_action( 'admin_menu', 'bulletproof_security_admin_menu' );
+    add_action( 'admin_menu', 'bulletproof_security_admin_link' );
 }
 
 function bps_plugin_actlinks( $links, $file ){
